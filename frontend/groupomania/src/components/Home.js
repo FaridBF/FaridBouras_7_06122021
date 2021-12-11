88f53476ea968026ab8d7d08620869749c777fc1
Bouras Farid <bouras.farid@outlook.fr>
2021-12-11 12:27:18 +0100

    feat: create Home and Profile components

---
+++ frontend/groupomania/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+  return (
+    <div className='all'>
+      Page d'accueil
+      <div>
+        <Button variant='primary'>Primary</Button>{' '}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
