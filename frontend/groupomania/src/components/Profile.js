5c40f92ef035e04ce35718cd68a3d45875cd122a
Bouras Farid <bouras.farid@outlook.fr>
2021-12-16 01:13:02 +0100

    feat: manage picture profile

--- frontend/groupomania/src/components/Profile.js
+++ frontend/groupomania/src/components/Profile.js
@@ -16,70 +16,64 @@ import ProfilImage from '../assets/images/profile1.jpg';
 
 const Profile = () => {
   return (
-    <div className='main-container'>
-      <Container className='profile-container'>
-        <Row>
-          <Col className='picture-container' xs={12} md={12} lg={4}>
-            <img
-              src={ProfilImage}
-              className='img-fluid rounded-circle'
-              alt="Visuel de l'utilisateur"
-            />
-            <Button
-              variant='primary'
-              type='submit'
-              aria-describedby='Connexion'
-              className='col-lg-5'
-            >
-              Modifier ma photo
-            </Button>
-          </Col>
-          <Col xs={8} md={8} lg={8}>
-            <p>Nom Prénom</p>
-            <p>Email</p>
-            <div className='d-flex'>
-              <InputGroup className='mb-3'>
-                <FormControl
-                  placeholder='Profession'
-                  aria-label='Profession du profil'
-                  aria-describedby='Profession du profil'
-                />
-                <Button variant='outline-secondary' id='button-addon2'>
-                  Modifier
-                </Button>
-              </InputGroup>
-            </div>
-            <div>
-              <InputGroup className='mb-3'>
-                <FormControl
-                  placeholder='Description'
-                  aria-label='Description du profil'
-                  aria-describedby='Description du profil'
-                />
-                <Button variant='outline-secondary' id='button-addon2'>
-                  Modifier
-                </Button>
-              </InputGroup>
-            </div>
-            <p>
-              <small>Vous disposez d'un compte admin</small>
-              <p>icône d'information à coller près d'admin</p>
-            </p>
-          </Col>
-        </Row>
-        <Row className='justify-content-center'>
-          <Col xs={2} md={3} lg={4}>
-            <Button
-              variant='outline-danger'
-              type='submit'
-              aria-describedby='Suppression'
-            >
-              Supprimer mon compte
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container className='main-container'>
+      <Row className='profile-container'>
+        <Col className='picture-container' xs={12} md={12} lg={4}>
+          <img
+            src={ProfilImage}
+            className='picture-profile img-fluid'
+            alt="Visuel de l'utilisateur"
+          />
+          <Button
+            variant='primary'
+            type='submit'
+            aria-describedby='Connexion'
+            className='col-lg-5'
+          >
+            Modifier ma photo
+          </Button>
+        </Col>
+        <Col xs={8} md={8} lg={8}>
+          <p>Nom Prénom</p>
+          <p>Email</p>
+          <div className='d-flex'>
+            <InputGroup className='mb-3'>
+              <FormControl
+                placeholder='Profession'
+                aria-label='Profession du profil'
+                aria-describedby='Profession du profil'
+              />
+              <Button variant='outline-secondary' id='button-addon2'>
+                Modifier
+              </Button>
+            </InputGroup>
+          </div>
+          <div>
+            <InputGroup className='mb-3'>
+              <FormControl
+                placeholder='Description'
+                aria-label='Description du profil' // nom du champs
+                aria-describedby='Description du profil' // description du champs
+              />
+              <Button variant='outline-secondary' id='button-addon2'>
+                Modifier
+              </Button>
+            </InputGroup>
+          </div>
+          <p>
+            <small>Vous disposez d'un compte admin</small>
+            <p>icône d'information à coller près d'admin</p>
+          </p>
+          <Button
+            variant='outline-danger'
+            type='submit'
+            aria-describedby='Suppression'
+          >
+            Supprimer mon compte
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
