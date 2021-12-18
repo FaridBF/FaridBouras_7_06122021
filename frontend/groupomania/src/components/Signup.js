528fa74a160df2acfb5023a3af8b20df3a656b0f
Bouras Farid <bouras.farid@outlook.fr>
2021-12-18 14:13:28 +0100

    style: correction css and added images

--- frontend/groupomania/src/components/Signup.js
+++ frontend/groupomania/src/components/Signup.js
@@ -14,63 +14,61 @@ import SignupImage from '../assets/images/signup.png';
  */
 const Signup = () => {
   return (
-    <div className='main-container-signup'>
-      <Container>
-        <Row className='align-items-center'>
-          <Col xs={12} md={6} lg={6}>
-            {/* <Image src='/assets/images/signup.png' fluid /> */}
-            <img
-              src={SignupImage}
-              className='img-fluid'
-              alt='Illustration de connexion'
-            />
-          </Col>
-          <Col xs={12} md={6} lg={6}>
-            <Form>
-              <Form.Group className='mb-3' controlId='formBasicEmail'>
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type='email'
-                  placeholder='Entrez votre adresse email'
-                />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Nom</Form.Label>
-                <Form.Control type='name' placeholder='Entrez votre Nom' />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Prénom</Form.Label>
-                <Form.Control type='name' placeholder='Entrez votre Prénom' />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Choisissez votre mot de passe</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Entrez votre mot de passe'
-                />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Confirmez votre mot de passe</Form.Label>
-                <Form.Control
-                  type='password'
-                  placeholder='Validez votre nouveau mot de passe'
-                />
-              </Form.Group>
-              <p>
-                <small>Vous avez déjà un compte ? Connectez-vous !</small>
-              </p>
-              <Button
-                variant='primary'
-                type='submit'
-                aria-describedby='Connexion'
-              >
-                S'inscrire
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container className='main-container-signup'>
+      <Row className='align-items-center'>
+        <Col xs={12} md={6} lg={6}>
+          {/* <Image src='/assets/images/signup.png' fluid /> */}
+          <img
+            src={SignupImage}
+            className='img-fluid'
+            alt='Illustration de connexion'
+          />
+        </Col>
+        <Col xs={12} md={6} lg={6}>
+          <Form>
+            <Form.Group className='mb-3' controlId='formBasicEmail'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Entrez votre adresse email'
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label>Nom</Form.Label>
+              <Form.Control type='name' placeholder='Entrez votre Nom' />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label>Prénom</Form.Label>
+              <Form.Control type='name' placeholder='Entrez votre Prénom' />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label>Choisissez votre mot de passe</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Entrez votre mot de passe'
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label>Confirmez votre mot de passe</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Validez votre nouveau mot de passe'
+              />
+            </Form.Group>
+            <p>
+              <small>Vous avez déjà un compte ? Connectez-vous !</small>
+            </p>
+            <Button
+              variant='primary'
+              type='submit'
+              aria-describedby='Connexion'
+            >
+              S'inscrire
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
