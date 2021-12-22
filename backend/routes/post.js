5513a2e035be2967266532a6d29017a7dda721c9
Bouras Farid <bouras.farid@outlook.fr>
2021-12-22 23:30:20 +0100

    feat : created "deletePost" controller

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const postCtrl = require('../controllers/post');
 
+// const auth = require('../middleware/auth');
+// const multer = require('../middleware/multer-config');
+
 //Routage
 router.post('/create', postCtrl.createPost);
 router.get('/:id', postCtrl.getPostById);
+
+router.delete('/:id', postCtrl.deletePost);
 
 module.exports = router;
