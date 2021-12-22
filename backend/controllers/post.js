5132dc026e4dbbf132c09723ba0b2f2466eaaa30
Bouras Farid <bouras.farid@outlook.fr>
2021-12-22 23:30:20 +0100

    feat : created "deletePost" controller

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -3,7 +3,7 @@ const db_connection = require('../config/database');
 /**
  * Création d'un post
  * @param  {content, image, link, create_time, update_time, user_id} req : informations reçues par le front
- * @param  {Code, message} res : réponse envoyée du back vers le front
+ * @param  {code, message} res : réponse envoyée du back vers le front
  */
 exports.createPost = (req, res) => {
   // si image ou link sont null, les supprimer
@@ -43,7 +43,27 @@ exports.getPostById = (req, res) => {
       res.status(400).json({ message: 'Une erreur est survenue.' });
       //   throw err;
     } else {
-      res.status(201).json(result[0]);
+      res.status(200).json(result[0]);
+    }
+  });
+};
+
+/**
+ * Suppression d'un post
+ * @param  {id} req : informations reçues par le front dans params
+ * @param  {code, message} res : réponse envoyée du back vers le front
+ */
+exports.deletePost = (req, res) => {
+  // TODO: vérifier droits (admin ou user propriétaire)
+  const sql_query = `DELETE FROM post WHERE id = ${req.params.id}`;
+  const db = db_connection.getDB();
+  db.query(sql_query, (err, result) => {
+    if (!result) {
+      res.status(400).json({ message: 'Une erreur est survenue.' });
+    } else {
+      res
+        .status(200)
+        .json({ message: 'Votre publication a été supprimée avec succès' });
     }
   });
 };
