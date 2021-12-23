9197b01cfa9d0d12e93e80292a76893f62e23525
Bouras Farid <bouras.farid@outlook.fr>
2021-12-23 12:56:39 +0100

    feat:  created "updateComment" and "deleteComment" controller

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -21,3 +21,44 @@ exports.createComment = (req, res) => {
     }
   });
 };
+
+/**
+ * Suppression d'un commentaire
+ * @param  {id} req : informations reçues par le front (id du comment dans params)
+ * @param  {code, message} res : réponse envoyée du back vers le front
+ */
+exports.deleteComment = (req, res) => {
+  const commentToDelete = {
+    ...req.body
+  };
+  const sql_query = `DELETE FROM comment WHERE id = ${req.params.id}`;
+  const db = db_connection.getDB();
+  db.query(sql_query, commentToDelete, (err, result) => {
+    if (!result) {
+      res.status(400).json({ message: 'Une erreur est survenue.' });
+    } else {
+      res
+        .status(201)
+        .json({ message: 'Votre commentaire a été supprimé avec succès !' });
+    }
+  });
+};
+
+/**
+ * Modification d'un commentaire
+ * @param  {id, content, update_time} req : informations reçues par le front (id du comment dans params)
+ * @param  {code, message} res : réponse envoyée du back vers le front
+ */
+exports.updateComment = (req, res) => {
+  const sql_query = `UPDATE comment SET content = "${req.body.content}", update_time = NOW() WHERE id = "${req.params.id}"`;
+  const db = db_connection.getDB();
+  db.query(sql_query, (err, result) => {
+    if (!result) {
+      res.status(400).json({ message: 'Une erreur est survenue.' });
+    } else {
+      res
+        .status(201)
+        .json({ message: 'Votre commentaire a été modifié avec succès !' });
+    }
+  });
+};
