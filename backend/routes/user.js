5e8c4cf8d06872c14431125d93afeff5ba7fda33
Bouras Farid <bouras.farid@outlook.fr>
2021-12-26 14:24:32 +0100

    feat: created "getUserDetails" and "setAdminuser" controller

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,10 +3,18 @@ const router = express.Router(); // création du routeur avec la fonction expres
 const userCtrl = require('../controllers/user');
 
 //Routage
+// inscripton et connexion
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
+// modififer et suppresion d'un user
 router.put('/:id', userCtrl.updateUser);
 router.delete('/:id', userCtrl.deleteUser);
+
+// paramètrage des droits (admin) d'un user
+router.put('/:id/adminrights', userCtrl.setAdminUser);
+
+// affichage de l'ensemble des données du User sans le mot de passe
+router.get('/:id', userCtrl.getUserDetails);
 
 module.exports = router;
