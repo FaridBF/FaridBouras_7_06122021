5eeb503e497d77d6f534a6bbd6356d771f941580
Bouras Farid <bouras.farid@outlook.fr>
2022-01-02 12:28:03 +0100

    feat-front: handle logout

--- frontend/groupomania/src/components/Header.js
+++ frontend/groupomania/src/components/Header.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -12,39 +16,62 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
  * Représente le header sur toutes les pages
  */
 const Header = () => {
-  const userIsConnected = true;
+  // récupère (state) infos user depuis Store
+  const userData = useSelector((state) => state.userReducer);
+  const handleLogout = (e) => {
+    e.preventDefault();
+    axios.get(`${process.env.REACT_APP_API_URL}api/user/logout`);
+    // Vider le store et faire la redirection vers page login
+    window.location = `/login`;
+  };
   return (
     <Container className='header-container' fluid>
       <Row>
         <Col xs={12} md={3} lg={3}>
-          <img
-            src={Logo}
-            className='img-fluid'
-            alt='Illustration du logo de la société'
-            height='180'
-            width='180'
-          />
+          <Link to='/home'>
+            <img
+              src={Logo}
+              className='img-fluid'
+              alt='Illustration du logo de la société'
+              height='180'
+              width='180'
+            />
+          </Link>
         </Col>
       </Row>
       <Row>
         <Col xs={12} md={3} lg={3}>
           {/* Gestion des boutons connexion/déconnexion */}
-          {userIsConnected === true ? (
-            <Button
-              className='button_connexion'
-              variant='primary'
-              size='lg'
-              active
-            >
-              <FontAwesomeIcon
-                className='icon_connexion'
-                icon='fa-solid fa-right-from-bracket'
-              />
-            </Button>
+          {userData.first_name !== undefined ? (
+            <>
+              <p>Hello, {userData.first_name}</p>
+              {/* TODO: ajouter icone profil */}
+              <Link to='/profile'>Profil</Link>
+              <Button
+                onClick={handleLogout}
+                className='button_connexion'
+                aria-label='Déconnexion'
+                variant='primary'
+                size='lg'
+                active
+              >
+                <FontAwesomeIcon
+                  className='icon_connexion'
+                  icon='fa-solid fa-right-from-bracket'
+                />
+              </Button>
+            </>
           ) : (
-            <Button variant='secondary' size='lg' active>
-              Connexion
-            </Button>
+            <Link to='/login'>
+              <Button
+                aria-label='Connexion'
+                variant='secondary'
+                size='lg'
+                active
+              >
+                Connexion
+              </Button>
+            </Link>
           )}
           {/* Fin gestion des boutons connexion/déconnexion */}
         </Col>
