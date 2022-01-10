676f7cc385083ded1cc08395f7fe614e1a71103e
Bouras Farid <bouras.farid@outlook.fr>
2022-01-10 23:13:49 +0100

    feat-front: display posts on 'homepage'

--- frontend/groupomania/src/components/Home.js
+++ frontend/groupomania/src/components/Home.js
@@ -1,5 +1,4 @@
 import Button from 'react-bootstrap/Button';
-
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -7,123 +6,94 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
-import ProfilImage from '../assets/images/profile1.jpg';
+import React, { useState, useEffect } from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Header from './Header';
+import PostsList from './PostsList';
+import Footer from './Footer';
+import ProfilImage from '../assets/images/profile1.jpg';
+import axios from '../api';
 
 /**
  * Représente la page principale avec les publications
  */
 const Home = () => {
+  const [newPost, setNewPost] = useState('');
+
+  const handleSubmitPost = (e) => {
+    e.preventDefault();
+    console.log('newPost: ', newPost);
+    // posts.append(newPost)
+    // console.log("newPostsList: ", posts)
+  };
+
+  // useEffect(() => {
+  //   const getPost = async () => {
+  //     await axios.get('post/17').then((res) => {
+  //       console.log('RES.DATA ', res.data);
+  //       setPosts(res.data[0]);
+  //       console.log(posts);
+  //     });
+  //   };
+  //   getPost();
+  // }, []);
   return (
-    <Container className='main-container justify-content-center'>
-      <Col xs={10} md={10} lg={10}>
-        {/* Création publication */}
-        <Row>
-          <Card>
-            <Card.Body>
-              <Row className='d-flex align-items-center'>
-                <Col xs={2} md={1} lg={1}>
-                  <img
-                    src={ProfilImage}
-                    className='picture-profile-publication img-fluid'
-                    alt="Visuel de l'utilisateur"
-                  />
-                </Col>
-                <Col className='author-date-publication'>
-                  <small className='publication-author'>Marie Dupont</small>
-                </Col>
-              </Row>
-              <Row>
-                <FloatingLabel
-                  controlId='floatingTextarea'
-                  label="Quoi de neuf aujourd'hui ?"
-                  // placeholder="Quoi de neuf aujourd'hui ?"
-                  className='mb-2 mt-3'
-                >
-                  <Form.Control
-                    className='new-publication'
-                    as='textarea'
-                    placeholder='Leave a comment here'
-                  />
-                </FloatingLabel>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Row>
-        {/* Fin création publication */}
-        {/* Première publication */}
-        <Row>
-          <Card>
-            <Card.Body>
-              <Row className='d-flex align-items-center'>
-                {/* <Card.Img variant='top' src='holder.js/100px180' /> */}
-                <Col xs={2} md={1} lg={1}>
-                  <img
-                    src={ProfilImage}
-                    className='picture-profile-publication img-fluid'
-                    alt="Visuel de l'utilisateur"
-                  />
-                </Col>
-                <Col className='author-date-publication'>
-                  <small className='publication-author'>Julie Leroy</small>
-                  <small className='publication-date'>12 Décembre 2021</small>
-                </Col>
-              </Row>
-              <Row className='publication-content'>
-                <Card.Text>Hey vous avez vu cet article ?</Card.Text>
-              </Row>
-              <Row className='publication-icons'>
-                <Col>
-                  <FontAwesomeIcon
-                    className='icon_thumbs'
-                    color='blue'
-                    icon='fa-solid fa-thumbs-up'
-                  />
-                  <FontAwesomeIcon color='blue' icon='fa-solid fa-message' />
-                </Col>
-              </Row>
-              {/* Commentaires */}
-              <Row className='d-flex align-items-center'>
-                <Col xs={2} md={1} lg={1}>
-                  <img
-                    src={ProfilImage}
-                    className='picture-profile-comment img-fluid'
-                    alt="Visuel de l'utilisateur"
-                  />
-                </Col>
-                <Col className='comment-text' xs={8} md={8} lg={8}>
-                  <p className='mb-0'>C'est génial !</p>
-                </Col>
-                <Col>
-                  <Button className='button_danger' variant='danger'>
-                    <FontAwesomeIcon icon='fa-solid fa-trash' />
-                  </Button>
-                </Col>
-              </Row>
-              {/* Fin commentaires */}
-              {/* Commentaire à écrire */}
-              <Row>
-                <FloatingLabel
-                  controlId='floatingTextarea'
-                  label="Quoi de neuf aujourd'hui ?"
-                  // placeholder="Quoi de neuf aujourd'hui ?"
-                  className='mb-2 mt-3'
-                >
-                  <Form.Control
-                    className='new-publication'
-                    as='textarea'
-                    placeholder='Leave a comment here'
-                  />
-                </FloatingLabel>
-              </Row>
-              {/* Fin commentaire à écrire */}
-            </Card.Body>
-          </Card>
-        </Row>
-        {/* Fin Première publication */}
-      </Col>
-    </Container>
+    <>
+      <Header />
+      <Container className='main-container justify-content-center'>
+        <Col xs={10} md={10} lg={10}>
+          {/* Création publication */}
+          <Row>
+            <Card>
+              <Card.Body>
+                <Row className='d-flex align-items-center'>
+                  <Col xs={2} md={1} lg={1}>
+                    <img
+                      src={ProfilImage}
+                      className='picture-profile-publication img-fluid'
+                      alt="Visuel de l'utilisateur"
+                    />
+                  </Col>
+                  <Col className='author-date-publication'>
+                    <small className='publication-author'>Marie Dupont</small>
+                  </Col>
+                </Row>
+                <Row>
+                  <Form onSubmit={handleSubmitPost}>
+                    <FloatingLabel
+                      controlId='floatingTextarea'
+                      label="Quoi de neuf aujourd'hui ?"
+                      className='mb-2 mt-3'
+                    >
+                      <Form.Control
+                        className='new-publication'
+                        as='textarea'
+                        placeholder='Entrez votre message'
+                        name='email'
+                        value={newPost}
+                        onChange={(e) => {
+                          setNewPost(e.target.value);
+                        }}
+                      />
+                    </FloatingLabel>
+                    <Button
+                      variant='primary'
+                      type='submit'
+                      aria-describedby='Connexion'
+                    >
+                      Publier
+                    </Button>
+                  </Form>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Row>
+          {/* Fin création publication */}
+          <PostsList />
+        </Col>
+      </Container>
+      <Footer />
+    </>
   );
 };
 
