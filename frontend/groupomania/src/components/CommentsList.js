38fbee098c48e813b4d52699480c5ef4566c9f3e
Bouras Farid <bouras.farid@outlook.fr>
2022-01-11 23:21:00 +0100

    docs: add code comments + correction props

--- frontend/groupomania/src/components/CommentsList.js
+++ frontend/groupomania/src/components/CommentsList.js
@@ -2,26 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from '../api';
 import Comment from './Comment';
 
-const CommentsList = (data) => {
+/**
+ * Représente le composant de la liste des commentaires d'une publication
+ * @param  {} props : id d'une publication
+ */
+const CommentsList = (props) => {
   // récupérer id de publication fourni en propriété par composant parent : Post
-  const post_id = data.post_id;
+  const post_id = props.post_id;
   const [comments, setComments] = useState([]);
-  console.log('comments', comments);
 
   useEffect(() => {
+    /**
+     * Requête à l'api pour récupérer la liste des commentaires d'une publication
+     */
     const getComments = async () => {
       await axios.get(`comment/${post_id}/all`).then((res) => {
-        // console.log('RES.DATA ', res.data);
         setComments(res.data);
       });
     };
     getComments();
-  }, []);
+  }, []); // tableau vide car on veut que cela ne se déclenche qu'à l'affichage du composant
+
   return (
     <>
       {/* Parcourir la liste des commentaires pour afficher chaqun */}
       {comments.map((comment) => {
-        return <Comment data={comment} key={comment.id} />;
+        return <Comment comment={comment} key={comment.id} />;
       })}
     </>
   );
--- frontend/groupomania/src/components/PostsList.js
+++ frontend/groupomania/src/components/PostsList.js
@@ -2,24 +2,29 @@ import React, { useState, useEffect } from 'react';
 import axios from '../api';
 import Post from './Post';
 
+/**
+ * Représente le composant de la liste des publications
+ */
 const PostsList = () => {
   const [posts, setPosts] = useState([]);
-  console.log('posts', posts);
 
   useEffect(() => {
+    /**
+     * Requête à l'api pour récupérer la liste des publications et infos des auteurs
+     */
     const getPosts = async () => {
       await axios.get('post/all').then((res) => {
-        console.log('RES.DATA ', res.data);
         setPosts(res.data);
       });
     };
     getPosts();
   }, []);
+
   return (
     <>
-      {/* Parcourir la liste des publications pour afficher chaqune */}
+      {/* Parcourir la liste des publications pour afficher chaqune en la passant dans les propriétés (props) du composant Post */}
       {posts.map((post) => {
-        return <Post data={post} key={post.id} />;
+        return <Post post={post} key={post.id} />;
       })}
     </>
   );
