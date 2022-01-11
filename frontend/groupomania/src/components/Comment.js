d8437a4264662541449e08954c53515c65c23a81
Bouras Farid <bouras.farid@outlook.fr>
2022-01-11 23:19:02 +0100

    fix: manage display comment when no data from back (ternaire)

--- frontend/groupomania/src/components/Comment.js
+++ frontend/groupomania/src/components/Comment.js
@@ -7,52 +7,61 @@ import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ProfilImage from '../assets/images/profile1.jpg';
 
-const Comment = (comment) => {
-  const currentCommentId = comment.data.id;
-  console.log('commentId: ', currentCommentId);
-  const [currentComment, setCurrentComment] = useState();
+/**
+ * Représente le composant d'un commentaire
+ * @param  {} props: objet représentant un commentaire
+ */
+const Comment = (props) => {
   // récupérer id de commentaire fourni en propriété par composant parent : CommentsList
+  const currentCommentId = props.comment.id;
+  const [currentComment, setCurrentComment] = useState();
 
   useEffect(() => {
+    /**
+     * Requête à l'api pour récupérer un commentaire par son id et des infos de l'auteur
+     */
     const getCommentById = async () => {
       await axios.get(`comment/${currentCommentId}`).then((res) => {
-        console.log('RES.DATA du commentaire ', res.data);
         setCurrentComment(res.data);
       });
     };
     getCommentById();
-  }, []);
+  }, []); // tableau vide car on veut que cela ne se déclenche qu'à l'affichage du composant
+
   return (
     <>
       {/* Première publication */}
       <Row>
         <Card>
           <Card.Body>
-            {/* Commentaires */}
-            <Row className='d-flex align-items-center'>
-              <Col xs={2} md={1} lg={1}>
-                {/* <img
-                  src={currentComment.picture}
-                  className='picture-profile-comment img-fluid'
-                  alt="Visuel de l'utilisateur"
-                /> */}
-              </Col>
-              <Col className='comment-text' xs={8} md={8} lg={8}>
-                {/* <p>
-                  {currentComment.first_name} {currentComment.last_name} -{' '}
-                  {currentComment.create_time}
-                </p>
-                <p className='mb-0'>{currentComment.content}</p> */}
-              </Col>
-              <Col>
-                <Button className='button_danger' variant='danger'>
-                  <FontAwesomeIcon icon='fa-solid fa-trash' />
-                </Button>
-              </Col>
-            </Row>
-            {/* Fin commentaires */}
+            {/* Commentaire */}
+            {currentComment ? (
+              <Row className='d-flex align-items-center'>
+                <Col xs={2} md={1} lg={1}>
+                  <img
+                    src={currentComment.picture}
+                    className='picture-profile-comment img-fluid'
+                    alt="Visuel de l'utilisateur"
+                  />
+                </Col>
+                <Col className='comment-text' xs={8} md={8} lg={8}>
+                  <p>
+                    {currentComment.first_name} {currentComment.last_name} -
+                    {currentComment.create_time}
+                  </p>
+                  <p className='mb-0'>{currentComment.content}</p>
+                </Col>
+                <Col>
+                  <Button className='button_danger' variant='danger'>
+                    <FontAwesomeIcon icon='fa-solid fa-trash' />
+                  </Button>
+                </Col>
+              </Row>
+            ) : (
+              ''
+            )}
+            {/* Fin commentaire */}
             {/* Commentaire à écrire */}
             <Row>
               <FloatingLabel
