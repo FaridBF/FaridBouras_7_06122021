b8d91ac6262fa205fb025896324fab4dac37b420
Bouras Farid <bouras.farid@outlook.fr>
2022-01-23 21:44:52 +0100

    feat: added get total likes by postId

--- frontend/groupomania/src/components/Post.js
+++ frontend/groupomania/src/components/Post.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -10,7 +10,7 @@ import CommentsList from './CommentsList';
 import { date_options } from '../utils/date';
 // import Card from 'react-bootstrap/Card';
 
-import { deletePost } from '../actions/post.actions';
+import { deletePost, getTotalPostLikes } from '../actions/post.actions';
 // import CardComments from './CardComment';
 
 /**
@@ -19,10 +19,12 @@ import { deletePost } from '../actions/post.actions';
  */
 const Post = (props) => {
   const currentPost = props.post;
+  const [totalLikes, setTotalLikes] = useState(0);
   // const [showComments, setShowComments] = useState(false);
   // récupérer infos de l'utilisateur depuis localstorage
   const userInfo = JSON.parse(localStorage.getItem('user_details'));
   const dispatch = useDispatch();
+  // const likes = useSelector((state) => state.postReducer); // récupérer likes du store
 
   /**
    * Supprime une publication
@@ -30,6 +32,33 @@ const Post = (props) => {
   const handleDeletePost = () => {
     dispatch(deletePost(currentPost.id));
   };
+
+  /**
+   * Récupère le total de likes de la publication depuis le store
+   * TODO: à revoir
+   */
+  // const handleTotalPostLikes = () => {
+  //   dispatch(getTotalPostLikes(currentPost.id));
+  // };
+
+  /**
+   * Récupère le total de likes de la publication depuis le back
+   */
+  const getTotalLikes = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}api/post/total-likes/${currentPost.id}`
+      )
+      .then((res) => {
+        setTotalLikes(res.data.TotalLikes);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    // handleTotalPostLikes();
+    getTotalLikes();
+  }, []);
 
   return (
     <>
@@ -95,7 +124,9 @@ const Post = (props) => {
               {/* Début affichage lien */}
               {currentPost.link != undefined || currentPost.link != null ? (
                 <>
-                  <Link to={currentPost.link}>{currentPost.link}</Link>
+                  <a target='_blank' href={currentPost.link}>
+                    {currentPost.link}
+                  </a>
                 </>
               ) : (
                 ''
@@ -105,14 +136,18 @@ const Post = (props) => {
             <Row className='publication-icons'>
               <Col>
                 <FontAwesomeIcon
-                  className='icon_thumbs'
-                  color='blue'
-                  icon='fa-solid fa-thumbs-up'
+                  // onClick={() => setShowComments(!showComments)}
+                  className='icon_add_comment m-2'
+                  icon='fa-solid fa-message'
                 />
                 <FontAwesomeIcon
-                  // onClick={() => setShowComments(!showComments)}
-                  color='blue'
-                  icon='fa-solid fa-message'
+                  className='icon_thumbs_up m-2'
+                  icon='fa-solid fa-thumbs-up'
+                />
+                <small>{totalLikes}</small>
+                <FontAwesomeIcon
+                  className='icon_thumbs_down m-2'
+                  icon='fa-solid fa-thumbs-down'
                 />
               </Col>
             </Row>
