3eaf5833dd8cc86002ad095dccf838dcffdfce2e
Bouras Farid <bouras.farid@outlook.fr>
2022-01-29 16:56:43 +0100

    feat-front: redux store for comments

--- frontend/groupomania/src/reducers/index.js
+++ frontend/groupomania/src/reducers/index.js
@@ -2,8 +2,10 @@
 import { combineReducers } from 'redux';
 import userReducer from './user.reducer';
 import postReducer from './post.reducer';
+import commentReducer from './comment.reducer';
 
 export default combineReducers({
   userReducer,
-  postReducer
+  postReducer,
+  commentReducer
 });
