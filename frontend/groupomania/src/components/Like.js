1f3b4fc382f467cb57f070e6943b8ab2d15b87dd
Bouras Farid <bouras.farid@outlook.fr>
2022-01-31 00:09:51 +0100

    feat-front: update display after like/unlike dislike/undislike

--- frontend/groupomania/src/components/Like.js
+++ frontend/groupomania/src/components/Like.js
@@ -7,25 +7,28 @@ import axios from 'axios';
  * @param  {likersList} props: liste des id des utilisateurs qui ont liké ce post
  */
 const Like = (props) => {
-  const [liked, setLiked] = useState(false);
-  const likersList = props.likersList;
+  // const [liked, setLiked] = useState(false);
+  const liked = props.liked;
+  const totalLikes = props.totalLikes;
+  // const likersList = props.likersList;
   const userInfo = JSON.parse(localStorage.getItem('user_details'));
   const currentPost = props.post;
 
   /**
    * Vérifier si user connecté a liké ce post
    */
-  const checkUserLikePost = () => {
-    // console.log(`liked? before check: ${liked} - post: ${currentPost.id}`);
-    likersList.map((user) => {
-      if (user.user_id === userInfo.id) {
-        setLiked(true);
-      } else {
-        setLiked(false);
-      }
-    });
-    // console.log(`liked? after check: ${liked} - post: ${currentPost.id}`);
-  };
+  // const checkUserLikePost = () => {
+  //   console.log(`liked? before check: ${liked} - post: ${currentPost.id}`);
+  //   console.log(likersList);
+  //   likersList.map((user) => {
+  //     if (user.user_id === userInfo.id) {
+  //       setLiked(true);
+  //     } else {
+  //       setLiked(false);
+  //     }
+  //   });
+  //   // console.log(`liked? after check: ${liked} - post: ${currentPost.id}`);
+  // };
 
   /**
    * Ajouter like
@@ -46,9 +49,10 @@ const Like = (props) => {
       .then((res) => {
         // si ok
         if (res.status === 201) {
-          setLiked(true);
+          props.setLiked(true);
           // raffraichir les likes en appelant la fonction du composant parent (Post)
-          props.getTotalLikes();
+          // props.getTotalLikes();
+          // checkUserLikePost();
         }
       })
       .catch((err) => console.log(err));
@@ -73,18 +77,28 @@ const Like = (props) => {
       .then((res) => {
         // si ok
         if (res.status === 200) {
-          setLiked(false);
           // raffraichir les likes en appelant la fonction du composant parent (Post)
-          props.getTotalLikes();
+          props.setLiked(false);
+          // checkUserLikePost();
+          // props.getTotalLikes();
         }
       })
       .catch((err) => console.log(err));
   };
 
-  useEffect(() => {
-    // vérifier si user connecté a liké ce post au chargement du composant
-    checkUserLikePost();
-  }, []);
+  // au chargement du composant
+  // useEffect(() => {
+  //   // vérifier si user connecté a liké ce post au chargement du composant
+  //   checkUserLikePost();
+  //   // props.getTotalLikes();
+  //   console.log('LIKEEED', liked);
+  // }, []);
+
+  // dès que 'liked' change de valeur
+  // useEffect(() => {
+  //   // raffraichir les likes en appelant la fonction du composant parent (Post)
+  //   props.getTotalLikes();
+  // }, [liked]);
 
   return (
     <>
@@ -94,6 +108,7 @@ const Like = (props) => {
         icon='fa-solid fa-thumbs-up'
         onClick={() => (liked ? unLike() : addLike())}
       />
+      <small>{totalLikes}</small>
     </>
   );
 };
