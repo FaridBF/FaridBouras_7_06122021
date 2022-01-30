cb5931dfa0e7ca790f65c53ccc918dafa2709ce2
Bouras Farid <bouras.farid@outlook.fr>
2022-01-31 00:09:51 +0100

    feat-front: update display after like/unlike dislike/undislike

--- frontend/groupomania/src/components/Dislike.js
+++ frontend/groupomania/src/components/Dislike.js
@@ -1,23 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 
 /**
  * Représente le 'dislike' d'un post
- * @param  {dislikersList} props: liste des id des utilisateurs qui ont disliké ce post
+ * @param {dislikersList} props: liste des id des utilisateurs qui ont disliké ce post
+ * @param {disliked} props: boolean pour savoir si user connecté a disliké ce post
+ * @param {totalDislikes} props: nombre total de dislike à ce post
  */
 const Dislike = (props) => {
-  const [disliked, setDisliked] = useState(false);
-  const dislikersList = props.dislikersList;
+  const disliked = props.disliked;
+  const totalDislikes = props.totalDislikes;
+  const currentPost = props.post;
   const userInfo = JSON.parse(localStorage.getItem('user_details'));
 
-  useEffect(() => {
-    // vérifier si user connecté a disliké ce post
-    dislikersList.map((user) => {
-      if (user.user_id === userInfo.id) {
-        setDisliked(true);
-      }
-    });
-  });
+  /**
+   * Ajouter dislike
+   */
+  const addDisLike = () => {
+    const data = {
+      user_id: userInfo.id,
+      post_id: currentPost.id,
+      type: -1
+    };
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}api/post/opinion/${currentPost.id}`,
+        data
+      )
+      .then((res) => {
+        // si ok
+        if (res.status === 201) {
+          // indiqué à Post (composant parent) que user connecté a disliké ce post
+          props.setDisliked(true);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  /**
+   * Retirer dislike
+   */
+  const unDislike = () => {
+    const data = {
+      user_id: userInfo.id,
+      post_id: currentPost.id,
+      type: 0
+    };
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}api/post/opinion/${currentPost.id}`,
+        data
+      )
+      .then((res) => {
+        // si ok
+        if (res.status === 200) {
+          // indiqué à Post (composant parent) que user connecté ne dislike plus ce post
+          props.setDisliked(false);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <FontAwesomeIcon
@@ -26,7 +70,10 @@ const Dislike = (props) => {
           disliked ? 'icon_thumbs_down_disliked m-2' : 'icon_thumbs_down m-2'
         }
         icon='fa-solid fa-thumbs-down'
+        // si user a déjà disliké ce post, appeler unDislike(), sinon, appeler addDislike()
+        onClick={() => (disliked ? unDislike() : addDisLike())}
       />
+      <small>{totalDislikes}</small>
     </>
   );
 };
