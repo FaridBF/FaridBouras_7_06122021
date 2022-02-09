c69253867f425b49d030658e3e4214505593d284
Bouras Farid <bouras.farid@outlook.fr>
2022-02-09 22:20:43 +0100

    feat-front: correction style "footer" and "home"

--- frontend/groupomania/src/components/Footer.js
+++ frontend/groupomania/src/components/Footer.js
@@ -14,21 +14,73 @@ library.add(fab);
  */
 
 const Footer = () => {
-  function handleClick(e) {
-    e.preventDefault();
-    console.log('Le lien a été cliqué.');
-  }
+  // function handleClick(e) {
+  //   e.preventDefault();
+  //   console.log('Le lien a été cliqué.');
+  // }
   return (
     <Container className='footer-container' fluid>
       <Row className='footer-style'>
-        <Col xs={12} md={3} lg={3} className='footer-position'>
-          Développé par Bouras Farid - Openclassrooms
-          <a
-            onClick={handleClick}
-            href='https://github.com/FaridBF/FaridBouras_7_06122021'
-          >
-            <FontAwesomeIcon icon={['fab', 'github']} className='footer-icon' />
-          </a>
+        <Col xs={12} md={12} lg={12} className='footer-position'>
+          <div className='logo-footer-position'>
+            <a
+              aria-label='Lien permettant de se connecter à Facebook'
+              target='_blank'
+              href='https://www.facebook.com/'
+            >
+              <FontAwesomeIcon
+                aria-label='Icône pour se connecter à Facebook'
+                icon={['fab', 'facebook']}
+                className='footer-icon'
+              />
+            </a>
+            <a
+              aria-label='Lien permettant de se connecter à Twitter'
+              target='_blank'
+              href='https://twitter.com/home'
+            >
+              <FontAwesomeIcon
+                aria-label='Icône permettant de se connecter à Twitter'
+                icon={['fab', 'twitter']}
+                className='footer-icon'
+              />
+            </a>
+            <a
+              aria-label='Lien permettant de se connecter à Youtube'
+              target='_blank'
+              href='https://www.youtube.com/'
+            >
+              <FontAwesomeIcon
+                aria-label='Icône permettant de se connecter à Youtube'
+                icon={['fab', 'youtube']}
+                className='footer-icon'
+              />
+            </a>
+            <a
+              aria-label='Lien permettant de se connecter à Github'
+              target='_blank'
+              href='https://github.com/FaridBF/FaridBouras_7_06122021'
+            >
+              <FontAwesomeIcon
+                aria-label='Icône permettant de se connecter à Github'
+                icon={['fab', 'github']}
+                className='footer-icon'
+              />
+            </a>
+          </div>
+          <div>
+            <ul className='total-link-footer'>
+              <a className='link-footer' href='http://localhost:3001/signup'>
+                <li>À propos</li>
+              </a>
+              <a className='link-footer' href='http://localhost:3001/signup'>
+                <li>Confidentialité</li>
+              </a>
+              <a className='link-footer' href='http://localhost:3001/signup'>
+                <li>Conditions générales</li>
+              </a>
+            </ul>
+          </div>
         </Col>
       </Row>
     </Container>
